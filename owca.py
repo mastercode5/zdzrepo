0c975801f9589d4e9fccbb98f08b85821e75fef6
Mastercode5 <dskybus@gmail.com>
2018-10-11 17:52:55 +0200

    fini

--- owca.py
+++ owca.py
@@ -1,1 +1,7 @@
 ileowiec = input("jak duża piramide z owiec mam narysowac: ")
+ileowiec = int(ileowiec)
+
+#zadanko dom napisac program ktory pobierze liczbe od uzytkownika i wyswietli piranide z  owiec
+# piramida kocia   =^.^=
+#               =^.^= =^.^= hint : ify i fory
+#zad dod zabespieczyc sie przed erroremktory wyskakuje gdy user do inputa napis a nie liczbe
